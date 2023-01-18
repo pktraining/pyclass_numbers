90c1fe361cb2044667d85f32269f581c9c8a78d9
Pierrick Roger <pierrick.rogermele@cea.fr>
2023-01-18 12:36:11 +0100

    Import example from Python class.

---
+++ fibo/__init__.py
@@ -0,0 +1,1 @@
+from .fcts import *
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='pyclass_numbers',
+    version='0.1.0',
+    description='Module example for my Python class',
+    author='Pierrick Roger',
+    author_email='pierrick.roger@gmail.com',
+    url='https://gitlab.com/pktraining/python-class/',
+    packages=['fibo']
+)
